326c067dd4077f54020a79432b8d5ac87f1922bd
Alexander Seleznev <seleznevam84@ya.ru>
2017-06-03 17:51:46 +0300

    change primary color

--- app/src/main/java/com/applications/whazzup/photomapp/data/managers/RealmManager.kt
+++ app/src/main/java/com/applications/whazzup/photomapp/data/managers/RealmManager.kt
@@ -4,10 +4,6 @@ import com.applications.whazzup.photomapp.data.network.res.PhotocardRes
 import com.applications.whazzup.photomapp.data.storage.realm.PhotocardRealm
 import io.realm.Realm
 
-/**
- * Created by Alex on 28.05.2017.
- */
-
 class RealmManager {
     fun savePhotocardResponseToRealm(photocard : PhotocardRes) {
         val realm = Realm.getDefaultInstance()
