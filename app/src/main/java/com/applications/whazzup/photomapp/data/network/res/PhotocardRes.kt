7d6e8d15ed0cce0c2e908b277dcffce65d3c5c14
Alexander Seleznev <seleznevam84@ya.ru>
2017-06-16 22:17:04 +0300

    Скруглил аватар

--- app/src/main/java/com/applications/whazzup/photomapp/data/network/res/PhotocardRes.kt
+++ app/src/main/java/com/applications/whazzup/photomapp/data/network/res/PhotocardRes.kt
@@ -1,4 +1,19 @@
 package com.applications.whazzup.photomapp.data.network.res
 
+
 data class PhotocardRes(val id: String, val owner: String, val title: String, val photo:String,
-                        val views: Int, val favorits: Int, val tags: List<String>, val filters: Map<String, String>)
+                        val views: Int, val favorits: Int, val tags: List<String>, val filters: Map<String, String>){
+
+
+
+    operator fun compareTo(o2: PhotocardRes?): Int {
+       if(o2!=null){
+           if(this.views>o2.views){
+               return 1
+           }else{
+               return -1
+           }
+       }
+        return -2
+    }
+}
